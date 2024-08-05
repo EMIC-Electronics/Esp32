26c496ff204e861ecd3b0c7a4b6f747fe4261565
mhunkeler <mariano.hunkeler@rfindustrial.com>
2024-08-05 20:01:58 -0300

    modelo

--- _api/WebServer/inc/webServer.h
+++ _api/WebServer/inc/webServer.h
@@ -10,7 +10,10 @@ WiFiUDP ntpUDP;
 NTPClient HTTP_timeClient(ntpUDP);
 
 char MQTT_TOPIC[32] = "usrr/prroj";                            /**< TOPIC where the photo and inferences will be posted. */
-char HTTP_URL[128] = "http://image-classifier.emic.io/predict"; /**< URL where the photo will be uploaded. */
+//char HTTP_URL[128] = "http://image-classifier.emic.io/predict"; /**< URL where the photo will be uploaded. */
+
+char HTTP_URL[128] = .{modelo}.; /* URL where the photo will be uploaded. */
+
 
 void HTTPInit(void);
 
